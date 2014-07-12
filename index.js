f4a24c6d7508d074c56a7cdf246fe62ab28033c1
LUO Minhui <hegfirose@gmail.com>
2014-07-12 23:14:23 +0800

    fix display page number when delta > current

--- index.js
+++ index.js
@@ -1,71 +1,88 @@
 /**
- * mongoose-paginator
+ * Mongoose Paginator
  */
 
 var Query = require('mongoose').Query;
 var render = require('./libs/render');
 
 /**
- * paginate
+ * paginator
  *
- * @param {Object} options
+ * @param options
+ * @param callback
  */
-Query.prototype.paginate = function(options, callback) {
-  var defaults = {
-    perPage: 10, // Number of items to display on each page.
-    delta  :  5, // Number of page numbers to display before and after the current one.
-    page   :  1,  // Initial page number.
-    params : {}
-  };
 
-  for(var key in options){
-    defaults[key] = options[key];
-  }
+Query.prototype.paginator = function(options, callback) {
+    var defaults = {
+        perPage: 10, // Number of items to display on each page.
+        delta: 5, // Number of page numbers to display before and after the current one.
+        page: 1,  // Initial page number.
+        params: {}
+    };
 
-  options = defaults;
+    for (var key in options) {
+        defaults[key] = options[key];
+    }
 
-  var delta = options.delta;
-  var current = page = parseInt(options.page, 10) || 1;
+    options = defaults;
 
-  var query = this;
-  var model = query.model;
-  model.count(query._conditions, function(err, count) {
-    var _skip = (current - 1) * options.perPage;
-    query.skip(_skip).limit(options.perPage).exec(function(err, results) {
-      if (err) {
-        callback(err, {});
-        return;
-      }
+    var delta = options.delta;
+    var current = page = parseInt(options.page, 10) || 1;
 
-      results = results || [];
+    var query = this;
+    var model = query.model;
+    model.count(query._conditions, function(err, count) {
+        var pagesCount = Math.ceil(count / options.perPage);
 
-      var last = Math.ceil(count / options.perPage);
-      
-      var start = Math.max(current - delta, 1);
-      var end   = Math.min(current + delta, last);
+        // req page should lte total pages
+        current = Math.min(current, pagesCount);
 
-      var pages = [];
-      for (var i = start; i <= end; i++) {
-        pages.push(i);
-      }
+        var _skip = (current - 1) * options.perPage;
+        query.skip(_skip).limit(options.perPage).exec(function(err, results) {
+            if (err) {
+                callback(err, {});
+                return;
+            }
 
-      var prev = Math.max(current - 1, start);
-      var next = Math.min(current + 1, end);
+            results = results || [];
 
-      var pager = {
-        'results': results,
-        'options': options,
-        'current': current,
-        'last': last,
-        'prev': prev,
-        'next': next,
-        'pages': pages,
-        'count': count
-      };
+            var start = Math.max(current - delta, 1);
+            var end = Math.min(current + delta, pagesCount);
 
-      pager['render'] = render(options);
+            var pagesReq = 2 * delta + 1;
 
-      callback(err, pager);
+            if (start === 1) {
+                end = pagesReq;
+            } else if (current < delta) {
+                end = pagesReq - start;
+            }
+
+            end = Math.min(end, pagesCount);
+
+            var pages = [];
+            for (var i = start; i <= end; i++) {
+                pages.push(i);
+            }
+
+            var prev = Math.max(current - 1, start);
+            var next = Math.min(current + 1, end);
+
+            console.log('end: %d, last: %d, prev: %d, next: %d', end, pagesCount, prev, next);
+
+            var pager = {
+                'results': results,
+                'options': options,
+                'current': current,
+                'last': pagesCount,
+                'prev': prev,
+                'next': next,
+                'pages': pages,
+                'count': count
+            };
+
+            pager['render'] = render(options);
+
+            callback(err, pager);
+        });
     });
-  });
 };
--- libs/render.js
+++ libs/render.js
@@ -23,5 +23,3 @@ module.exports = function(options){
         return render(options);
     };
 };
-
-
